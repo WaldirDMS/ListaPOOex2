cd46cdb2fbd274a91d8b802e23cffbf8e670a8bb
WaldirDMS <75144213+WaldirDMS@users.noreply.github.com>
2020-11-27 18:59:00 -0300

    Add files via upload

---
+++ Lista1POOex2/Lista1POOex2/Quadrado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lista1POOex2
+{
+    class Quadrado
+    {
+        
+        private int area;
+        private int aresta;
+
+        public void setAresta(int p)
+        {
+            aresta = p;
+        } 
+
+        public int getAresta()
+        {
+            return aresta;
+        }
+
+        public int getArea()
+        {
+            return area;
+        }
+
+        public void calcularArea()
+        {
+            area = aresta * aresta;
+        }
+    }
+}
